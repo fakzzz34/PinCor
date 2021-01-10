003c26c227418da8e8855f70af32f9631707753b
fakzzz34 <59702013+fakzzz34@users.noreply.github.com>
2021-01-10 14:17:49 +0700

    Add files via upload

---
+++ LoadJSON.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using UnityEngine.Networking;
+using TMPro;
+
+public class LoadJSON : MonoBehaviour
+{
+    public TMP_Text jumlah_positif;
+    public TMP_Text jumlah_sembuh;
+    public TMP_Text jumlah_meninggal;
+
+    private void Start() {
+        GetJSONData();
+    }
+
+    // Start is called before the first frame update
+    public void GetJSONData()
+    {
+        StartCoroutine(RequestWebServices());
+    }
+
+    IEnumerator RequestWebServices() {
+        string getDataURL = "https://data.covid19.go.id/public/api/update.json";
+        print(getDataURL);
+
+        using (UnityWebRequest webData = UnityWebRequest.Get(getDataURL)) 
+        {
+
+            yield return webData.SendWebRequest();
+            if(webData.isNetworkError || webData.isHttpError) 
+            {
+                print("Error : " + webData.error);
+            }
+            else
+            {
+                if (webData.isDone)
+                {
+                    JSONNode jsonData = JSON.Parse(System.Text.Encoding.UTF8.GetString(webData.downloadHandler.data));
+
+                    if (jsonData == null)
+                    {
+                        print("------NO DATA------");
+                    }
+                    else
+                    {
+                        print("-----JSON DATA-----");
+                        print("jsonData.count : " + jsonData.Count);
+                        jumlah_positif.text = jsonData["update"]["total"]["jumlah_positif"].Value;
+                        jumlah_sembuh.text = jsonData["update"]["total"]["jumlah_sembuh"].Value;
+                        jumlah_meninggal.text = jsonData["update"]["total"]["jumlah_meninggal"].Value;
+                        
+                    }
+                }
+            }
+        }
+    }
+}
